67a13247171e5ae0d751e91b7a1755aefc6ab1ae
Kuntal <theencodedbong@gmail.com>
2026-04-20 23:19:13 +0100

    Added data source exclusion

--- restful-web-services-security/security-jwt/jwt-advanced/src/test/java/com/teb/practice/JwtAdvancedAppTest.java
+++ restful-web-services-security/security-jwt/jwt-advanced/src/test/java/com/teb/practice/JwtAdvancedAppTest.java
@@ -3,7 +3,11 @@ package com.teb.practice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+            "spring.flyway.enabled=false",
+            "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+        })
 class JwtAdvancedAppTest {
 
     @Test
