985632891c8ed8f1b96409a6371a3a258a3e2e1a
Kuntal <theencodedbong@gmail.com>
2026-04-20 23:24:30 +0100

    Removed exclusion from classes using database

--- restful-web-services-security/security-jwt/jwt-advanced/src/test/java/com/teb/practice/JwtAdvancedAppTest.java
+++ restful-web-services-security/security-jwt/jwt-advanced/src/test/java/com/teb/practice/JwtAdvancedAppTest.java
@@ -3,11 +3,7 @@ package com.teb.practice;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(
-        properties = {
-            "spring.flyway.enabled=false",
-            "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
-        })
+@SpringBootTest(properties = {"spring.flyway.enabled=false"})
 class JwtAdvancedAppTest {
 
     @Test
