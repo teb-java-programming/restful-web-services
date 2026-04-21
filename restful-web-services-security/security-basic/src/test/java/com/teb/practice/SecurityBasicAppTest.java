fd6a1b496d8892cd07430c945c8f35df10c44c19
Kuntal <theencodedbong@gmail.com>
2026-04-21 22:18:31 +0100

    Adding spring boot tests

---
+++ restful-web-services-security/security-basic/src/test/java/com/teb/practice/SecurityBasicAppTest.java
@@ -0,0 +1,15 @@
+package com.teb.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(
+        properties = {
+            "spring.flyway.enabled=false",
+            "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+        })
+class SecurityBasicAppTest {
+
+    @Test
+    void contextLoads() {}
+}
---
+++ restful-web-services-security/security-jwt/jwt-advanced/src/test/java/com/teb/practice/JwtAdvancedAppTest.java
@@ -0,0 +1,11 @@
+package com.teb.practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(properties = {"spring.flyway.enabled=false"})
+class JwtAdvancedAppTest {
+
+    @Test
+    void contextLoads() {}
+}
