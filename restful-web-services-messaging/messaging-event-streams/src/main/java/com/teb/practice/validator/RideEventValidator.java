f17731375b10a76eeba46c1458f319242e5a91ba
Kuntal <theencodedbong@gmail.com>
2026-05-18 18:37:03 +0100

    Code clean up

--- restful-web-services-messaging/messaging-event-streams/src/main/java/com/teb/practice/validator/RideEventValidator.java
+++ restful-web-services-messaging/messaging-event-streams/src/main/java/com/teb/practice/validator/RideEventValidator.java
@@ -1,11 +1,8 @@
 package com.teb.practice.validator;
 
-import static com.teb.practice.model.RideStatus.ASSIGNED;
 import static com.teb.practice.model.RideStatus.CANCELLED;
-import static com.teb.practice.model.RideStatus.COMPLETED;
 import static com.teb.practice.model.RideStatus.PAYMENT_COMPLETED;
 import static com.teb.practice.model.RideStatus.REQUESTED;
-import static com.teb.practice.model.RideStatus.STARTED;
 
 import static java.util.Objects.isNull;
 
@@ -15,22 +12,8 @@ import com.teb.practice.model.RideStatus;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-import java.util.Set;
-
 @Component
 public class RideEventValidator {
-
-    private static final Map<RideStatus, Set<RideStatus>> VALID_TRANSITIONS =
-            Map.of(
-                    REQUESTED,
-                    Set.of(ASSIGNED, CANCELLED),
-                    ASSIGNED,
-                    Set.of(STARTED, CANCELLED),
-                    STARTED,
-                    Set.of(COMPLETED),
-                    COMPLETED,
-                    Set.of(PAYMENT_COMPLETED));
 
     public boolean isValid(Ride ride, RideEvent event) {
 
@@ -45,6 +28,6 @@ public class RideEventValidator {
         if (CANCELLED.equals(currentStatus) || PAYMENT_COMPLETED.equals(currentStatus))
             return false;
 
-        return VALID_TRANSITIONS.getOrDefault(currentStatus, Set.of()).contains(newStatus);
+        return currentStatus.canTransitionTo(newStatus);
     }
 }
